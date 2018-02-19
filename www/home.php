95c570c301bd7b88a1f368b38598c8f2ad1c86ff
Jérémy Dos santos <jeremy.dos-santos@epita.fr>
2018-02-20 00:30:15 +0100

    www : renamed src dir

---
+++ www/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+ 
+  <head>
+    <title> BlindTest </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel='stylesheet' href='home.css' type='text/css' />
+    <script type="text/javascript" src="home.js"></script>
+  </head>
+
+  <body> 
+  </body>
+
+</html>
